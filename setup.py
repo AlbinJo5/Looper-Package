2bac359de73ba3adbedafb49c05aedca035aeef3
AlbinJo5 <albinjo12345alex@gmail.com>
2022-11-08 17:13:50 +0530

    Pacakge  Looper Python

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+ name='cartesian-looper', 
+ version='0.1',
+ author="Albin Jo Alex",
+ author_email="albinjo12345alex@gmail.com",
+ description="Loop through List and Dictionary to return the cartesian product ",
+ long_description="Loop through List and Dictionary to return the cartesian product, loopList() method returns cartesian product of different list, loopDictionary() method returns cartesian product of dictionary items with their name  ",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
